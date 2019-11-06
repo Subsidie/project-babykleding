bf27c24f0457e622908aafd4916975c5d413e4e1
Dylan van Schaik <dvs.am1c.ionic@gmail.com>
2019-11-06 14:36:07 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Icons font-awesome -->
+    <script src="https://kit.fontawesome.com/d124c0bd77.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./css/style.css"
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="zoeken" placeholder="Zoeken" aria-label="zoeken">
+                <button class="btn btn-dark my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+            </form>
+        </ul>
+        <ul class="navbar-nav navbar-right">
+            <li class="nav-item">
+                <a class="nav-link" id="inloggen" href="#"><i class="fas fa-user"></i> Inloggen / Registreren</a>
+            </li>
+            <li class="nav-item>">
+                <a class="nav-link" id="winkelmand" href="#"><i class="fas fa-shopping-basket"></i></a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<nav class="navbar navbar-expand-sm" id="navonder">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                <a class="nav-link" id="catalogus" href="#" ><i class="fas fa-bars"></i> Catalogus</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="contact" href="#">Contact</a>
+                </li>
+                <li class="nav-item>">
+                    <a class="nav-link" id="over" href="#">Over</i></a>
+                </li>
+                <li class="nav-item>">
+                    <a class="nav-link" id="beurs" href="#">Beurs</i></a>
+                </li>
+        </ul>
+    </div>
+</nav>
+
+    <div class="uitverkoop">★ GRATIS LEVERING VANAF € 49,- ★</div>
+
+    <div class="container-fluid">
+        <div class="row Slider">
+            <div class="col-sm-12">
+                <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-interval="10000">
+                            <a href="#"><img src="./img/teasers/winterverkoop.png" class="d-block w-100" id="winteruitverkoop"></a>
+                        </div>
+                        <div class="carousel-item" data-interval="10000">
+                            <a href="#"><img src="./img/teasers/disneyfans.png" class="d-block w-100"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="cont-teasers">
+            <div class="row row-Teasers">
+                <div class="col-md-4">
+                    <img src="./img/teasers/manholdingbaby.png">
+                </div>
+                <div class="col-md-4">
+                    <img src="./img/teasers/woman0year.png">
+                </div>
+                <div class="col-md-4">
+                    <img src="./img/teasers/babyspullen.png">
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+<script src="./js/app.js"></script>
+</body>
+
+</html>
